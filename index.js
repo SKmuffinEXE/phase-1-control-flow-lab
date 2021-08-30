2a4333a363f55d00e1cfde35a3672ff110a386cf
David <DEvangelista54@gmail.com>
2021-08-30 16:58:27 -0400

    commit tree a482d1085032d64d5a2a9c41be481d19b4fe0d0a

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+  
+  let greeting = '';
+
+  if (ride <= 400){
+    greeting = 'This one is on me!';
+  }
+  else if(ride > 2000 && ride < 2500){
+    greeting = 'I will gladly take your thirty bucks.'
+  }
+  else{
+    greeting = 'No can do.';
+  }
+
+  return(greeting);
+  
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+let cityCheck = (city === "NYC") ? "Ok, sounds good.": "No go.";
+return(cityCheck);
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return("Thank you so much.")
+      break;
+    case "not as generous":
+      return("Thank you.")
+      break;
+    default:
+      return("Bye.")
+  }
 }
